7d826a626f742d719f58f3d3b767ff786741083f
DA-8 <64353756+DA-8@users.noreply.github.com>
2020-05-16 00:38:48 +0300

    Add files via upload

---
+++ HW5/Q05.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Q05 {
+
+	public static void main(String[] args) {
+		
+//		printStormname("Arlene");
+//		printStormname(new String[] {"Bret"});
+//		printStormNames("Arlene");
+//		printStormNames(new String[] {"Bret"});
+		
+		
+	}
+	public void printStormname(String...names) {
+		System.out.println(Arrays.toString(names));
+	}
+	public void printStormNames(String[] names) {
+		System.out.println(Arrays.toString(names));
+	}
+
+
+}
