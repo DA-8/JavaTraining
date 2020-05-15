d25923205fd1b9198f27be5b1b63d8b922f41450
DA-8 <64353756+DA-8@users.noreply.github.com>
2020-05-16 00:14:37 +0300

    Add files via upload

---
+++ HW5/Q03.java
@@ -0,0 +1,18 @@
+
+public class Q03 {
+ 
+	public static void main(String[] args) {
+		int i=11;
+		do {
+			
+			if(i>5) {
+				System.out.println("i: "+i);
+			}
+			i++;
+		} while(i<10);
+		
+
+	}
+}
+
+//i: 11
