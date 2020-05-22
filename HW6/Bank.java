a00a795d2f5f0f8a166ffcebfc1f6c3d2f484f2b
DA-8 <64353756+DA-8@users.noreply.github.com>
2020-05-22 19:30:40 +0300

    Add files via upload

---
+++ HW6/Bank.java
@@ -0,0 +1,7 @@
+package wardrobe;
+
+public class Bank {
+	public static void withdrawal(int amountInCents) {}
+	public static void deposit(int amountInCents) {}
+	
+}
---
+++ HW6/clothes.java
@@ -0,0 +1,7 @@
+package wardrobe;
+
+public class clothes {
+
+	public static String getClothes() { return "dress"; }
+
+}
